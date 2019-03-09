4e046b18d82b2eb73cde1a748836a7834078ecaa
boretti <mathieu.boretti@gmail.com>
2019-03-09 15:13:49 +0100

    Update javadoc

--- src/main/java/ch/powerunit/extensions/async/WaitResult.java
+++ src/main/java/ch/powerunit/extensions/async/WaitResult.java
@@ -26,7 +26,12 @@ public final class WaitResult {
 	 * Start the builder to create an instance of {@link CompletableFuture} based on
 	 * the result of the received action, with repetition until some condition.
 	 * <p>
-	 * TODO
+	 * <h1>Simple sample</h1>
+	 * 
+	 * <pre>
+	 * CompletableFuture&lt;Optional&lt;MyObject&gt;&gt; exec = WaitResult.of(myObject::myCallable).expecting(myObject::myControl)
+	 * 		.repeat(10).every(10, TimeUnit.MILLISECONDS).asyncExec();
+	 * </pre>
 	 * 
 	 * @param action
 	 *            the {@link Callable} providing the result.
@@ -42,7 +47,12 @@ public final class WaitResult {
 	 * Start the builder to create an instance of {@link CompletableFuture} based on
 	 * repeated control on the mutable object.
 	 * <p>
-	 * TODO
+	 * <h1>Simple sample</h1>
+	 * 
+	 * <pre>
+	 * CompletableFuture&lt;Optional&lt;MyObject&gt;&gt; exec = WaitResult.on(myObject).expecting(MyObject::myControl).repeat(100)
+	 * 		.every(10, TimeUnit.MILLISECONDS).asyncExec();
+	 * </pre>
 	 * 
 	 * @param mutableObject
 	 *            the mutable object to be checked.
