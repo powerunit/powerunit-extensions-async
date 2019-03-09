b1bb639aae6c5bf32f2b4bb3f6fdc91610139777
boretti <mathieu.boretti@gmail.com>
2019-03-09 14:23:50 +0100

    Add file for travis ci

--- src/main/java/ch/powerunit/extensions/async/lang/WaitResultBuilder3.java
+++ src/main/java/ch/powerunit/extensions/async/lang/WaitResultBuilder3.java
@@ -4,6 +4,7 @@
 package ch.powerunit.extensions.async.lang;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Third Step of the builder of {@link CompletableFuture} to specify the maximal
@@ -22,4 +23,14 @@ public interface WaitResultBuilder3<T> {
 	 * @return {@link WaitResultBuilder4 the next step of the builder}
 	 */
 	WaitResultBuilder4<T> repeat(int count);
+
+	/**
+	 * Specify that only one retry will be done (so only one execution and one
+	 * validation).
+	 * 
+	 * @return {@link WaitResultBuilder5 the final step of the builder}
+	 */
+	default WaitResultBuilder5<T> repeatOnlyOnce() {
+		return repeat(1).every(1, TimeUnit.MILLISECONDS);
+	}
 }
