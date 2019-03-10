79c1d556fd519517ade417cb11e6bda090dffd24
boretti <mathieu.boretti@gmail.com>
2019-03-10 10:36:08 +0100

    Initial commit for issue #5

---
+++ src/main/java/ch/powerunit/extensions/async/impl/ExceptionHandler.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package ch.powerunit.extensions.async.impl;
+
+/**
+ * @author borettim
+ *
+ */
+class ExceptionHandler {// package protected
+	private final boolean ignoreException;
+
+	private final boolean alsoDontThrowLastExceptionWhenNoResult;
+
+	public ExceptionHandler(boolean ignoreException, boolean alsoDontThrowLastExceptionWhenNoResult) {
+		this.ignoreException = ignoreException;
+		this.alsoDontThrowLastExceptionWhenNoResult = alsoDontThrowLastExceptionWhenNoResult;
+	}
+
+	public void handleException(Exception e) {
+		if (!ignoreException) {
+			throw new AssertionError("Unable to obtains the result during one try, because of " + e.getMessage()
+					+ " ; Original error class is " + e.getClass(), e);
+		}
+	}
+
+	public void handleFinalException(Exception e) {
+		if (e != null && !alsoDontThrowLastExceptionWhenNoResult) {
+			throw new AssertionError("Unable to obtains the result and finish in error, because of " + e.getMessage()
+					+ " ; Original error class is " + e.getClass(), e);
+		}
+	}
+}
