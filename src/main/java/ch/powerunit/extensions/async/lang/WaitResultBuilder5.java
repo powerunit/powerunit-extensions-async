50ce4f19f81e20ffcf1607b021fb9f2f6eb1c7d7
boretti <mathieu.boretti@gmail.com>
2019-03-10 09:42:45 +0100

    Initial commit for issue #2

--- src/main/java/ch/powerunit/extensions/async/lang/WaitResultBuilder5.java
+++ src/main/java/ch/powerunit/extensions/async/lang/WaitResultBuilder5.java
@@ -5,6 +5,7 @@ package ch.powerunit.extensions.async.lang;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ForkJoinPool;
 
@@ -28,13 +29,49 @@ public interface WaitResultBuilder5<T> {
 	CompletableFuture<Optional<T>> asyncExec(Executor executor);
 
 	/**
-	 * Create ahd start the async execution of the {@link CompletableFuture} using
+	 * Create and start the async execution of the {@link CompletableFuture} using
 	 * {@link ForkJoinPool#commonPool()}.
 	 * 
 	 * @return the {@link CompletableFuture}
 	 */
 	default CompletableFuture<Optional<T>> asyncExec() {
 		return asyncExec(ForkJoinPool.commonPool());
+	}
+
+	/**
+	 * Directly wait for the result of this execution. In case of not ignored
+	 * exception, an {@link AssertionError} is thrown.
+	 * 
+	 * @return the {@link Optional} with the result of the execution
+	 * 
+	 * @throws AssertionError
+	 *             In case of not ignored exception.
+	 */
+	default Optional<T> finish() {
+		try {
+			return asyncExec().get();
+		} catch (InterruptedException e) {
+			throw new AssertionError("Unable to get the result, because of " + e.getMessage(), e);
+		} catch (ExecutionException e) {
+			if (e.getCause() instanceof AssertionError) {
+				throw (AssertionError) e.getCause();
+			}
+			throw new AssertionError("Unexpected error " + e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * Directly wait for a positive result of this execution. In case of not ignored
+	 * exception, or when no result are available, an {@link AssertionError} is
+	 * thrown.
+	 * 
+	 * @return the value if available
+	 * 
+	 * @throws AssertionError
+	 *             In case of not ignored exception.
+	 */
+	default T finishWithAResult() {
+		return finish().orElseThrow(() -> new AssertionError("No result is available"));
 	}
 
 }
--- src/test/java/ch/powerunit/extensions/async/WaitResultTest.java
+++ src/test/java/ch/powerunit/extensions/async/WaitResultTest.java
@@ -7,12 +7,15 @@ import java.util.concurrent.TimeUnit;
 
 import ch.powerunit.Test;
 import ch.powerunit.TestSuite;
+import ch.powerunit.extensions.async.lang.WaitResultBuilder5;
 
 public class WaitResultTest implements TestSuite {
 
 	private static class Handler {
 		public boolean ok = false;
 	}
+
+	// async
 
 	@Test
 	public void testObjectMethodDirectlyOK() throws InterruptedException, ExecutionException {
@@ -43,14 +46,66 @@ public class WaitResultTest implements TestSuite {
 		}).expecting(o -> true).repeat(10).every(10, TimeUnit.MILLISECONDS).asyncExec();
 		assertWhen(exec::get).throwException(exceptionMessage(containsString("TEST")));
 	}
-	
+
 	@Test
-	public void tesIgnoreExceptionThenAbsent() throws InterruptedException, ExecutionException {
+	public void testIgnoreExceptionThenAbsent() throws InterruptedException, ExecutionException {
 		CompletableFuture<Optional<Object>> exec = WaitResult.of(() -> {
 			throw new IllegalArgumentException("TEST");
 		}).ignoreException().expecting(o -> true).repeat(10).every(10, TimeUnit.MILLISECONDS).asyncExec();
 		Optional<Object> result = exec.get();
 		assertThat(result).isNotNull();
 		assertThat(result.isPresent()).is(false);
+	}
+
+	// finish
+
+	@Test
+	public void testObjectMethodDirectlyOKWithFinish() {
+		Optional<Object> result = WaitResult.on(new Object()).expecting(o -> true).repeat(100)
+				.every(10, TimeUnit.MILLISECONDS).finish();
+		assertThat(result).isNotNull();
+		assertThat(result.isPresent()).is(true);
+	}
+
+	@Test
+	public void testNotIgnoreExceptionThenExceptionWithFinish() {
+		WaitResultBuilder5<Object> exec = WaitResult.of(() -> {
+			throw new IllegalArgumentException("TEST");
+		}).expecting(o -> true).repeat(10).every(10, TimeUnit.MILLISECONDS);
+		assertWhen(exec::finish).throwException(exceptionMessage(containsString("TEST")));
+	}
+
+	@Test
+	public void testIgnoreExceptionThenAbsentWithFinish() {
+		Optional<Object> result = WaitResult.of(() -> {
+			throw new IllegalArgumentException("TEST");
+		}).ignoreException().expecting(o -> true).repeat(10).every(10, TimeUnit.MILLISECONDS).finish();
+		assertThat(result).isNotNull();
+		assertThat(result.isPresent()).is(false);
+	}
+
+	// finishWithAError
+
+	@Test
+	public void testObjectMethodDirectlyOKWithFinishWithAResult() {
+		Object result = WaitResult.on(new Object()).expecting(o -> true).repeat(100).every(10, TimeUnit.MILLISECONDS)
+				.finishWithAResult();
+		assertThat(result).isNotNull();
+	}
+
+	@Test
+	public void testNotIgnoreExceptionThenExceptionWithFinishAResult() {
+		WaitResultBuilder5<Object> exec = WaitResult.of(() -> {
+			throw new IllegalArgumentException("TEST");
+		}).expecting(o -> true).repeat(10).every(10, TimeUnit.MILLISECONDS);
+		assertWhen(exec::finishWithAResult).throwException(exceptionMessage(containsString("TEST")));
+	}
+
+	@Test
+	public void testIgnoreExceptionThenFailureWithFinish() {
+		WaitResultBuilder5<Object> exec = WaitResult.of(() -> {
+			throw new IllegalArgumentException("TEST");
+		}).ignoreException().expecting(o -> true).repeat(10).every(10, TimeUnit.MILLISECONDS);
+		assertWhen(exec::finishWithAResult).throwException(instanceOf(AssertionError.class));
 	}
 }
